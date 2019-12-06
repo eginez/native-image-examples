a6157bbaca0c2aeffdcc0f055dac55249bc6203d
Esteban Ginez <esteban.ginez@oracle.com>
2019-12-06 13:00:51 -0800

    Gradle changes to run with agent, blank config files

--- rmi/src/main/java/org/graalvm/nativeimage/demos/rmi/Main.java
+++ rmi/src/main/java/org/graalvm/nativeimage/demos/rmi/Main.java
@@ -13,7 +13,7 @@ public class Main {
       System.out.println("staring server");
       LocateRegistry.createRegistry(Integer.parseInt(port));
       final String serverUrl = String.format("rmi://localhost:%s/encoder", port);
-      //Naming.rebind(serverUrl, new Server());
+      Naming.rebind(serverUrl, new Server());
       System.out.println("waiting for connections");
    }
 }
