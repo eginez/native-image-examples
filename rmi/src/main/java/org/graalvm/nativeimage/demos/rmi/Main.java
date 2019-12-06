ea904c7c9d1c72784f93c96a7e384fbe01d13fb0
Esteban Ginez <esteban.ginez@oracle.com>
2019-12-05 16:13:35 -0800

    Adding native-image properties and build options

--- rmi/src/main/java/org/graalvm/nativeimage/demos/rmi/Main.java
+++ rmi/src/main/java/org/graalvm/nativeimage/demos/rmi/Main.java
@@ -2,17 +2,18 @@ package org.graalvm.nativeimage.demos.rmi;
 
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
+import java.util.Random;
 
 public class Main {
    public static void main(String[] args) throws Exception  {
-      startServer("7000");
+      new Main().startServer("7000");
    }
 
-   public static void startServer(String port) throws Exception {
+   public void startServer(String port) throws Exception {
       System.out.println("staring server");
       LocateRegistry.createRegistry(Integer.parseInt(port));
       final String serverUrl = String.format("rmi://localhost:%s/encoder", port);
-      Naming.rebind(serverUrl, new Server());
+      //Naming.rebind(serverUrl, new Server());
       System.out.println("waiting for connections");
    }
 }
